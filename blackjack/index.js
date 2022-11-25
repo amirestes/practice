596f5eb41370397cd4f9472ea6df8b599895dcb4
Alice Mirestes <amirestes@gmail.com>
2022-11-24 18:12:28 -0600

    updated else if statement

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,16 +1,16 @@
-// 1. Create two variables, firstCard and secondCard. 
+// 1. Create two variables, firstCard and secondCard.
 // Set their values to a random number between 2-11
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 6
-let secondCard = 9
-let sum = firstCard + secondCard
+let firstCard = 6;
+let secondCard = 9;
+let sum = firstCard + secondCard;
 
 if (sum < 21) {
-    console.log("Do you want to draw a new card? 🙂")
+  console.log("Do you want to draw a new card? 🙂");
 } else if (sum === 21) {
-    console.log("Woohoo! You've got Blackjack! 🥳")
-} else if (sum > 21) {
-    console.log("You're out of the game! 😭")
+  console.log("Woohoo! You've got Blackjack! 🥳");
+} else {
+  console.log("You're out of the game! 😭");
 }
