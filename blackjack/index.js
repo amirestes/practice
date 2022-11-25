1f7bd2cfd579265dba6852300dbd5cddc47ad8fc
Alice Mirestes <amirestes@gmail.com>
2022-11-25 09:57:39 -0600

    created renderGame() and adjusted startGame()

--- blackjack/index.js
+++ blackjack/index.js
@@ -9,6 +9,9 @@ let sumEl = document.getElementById("sum-el");
 let cardsEl = document.querySelector("#cards-el");
 
 function startGame() {
+  renderGame();
+}
+function renderGame() {
   cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
 
   sumEl.textContent = "Sum: " + sum;
@@ -28,5 +31,5 @@ function newCard() {
   // console.log("Drawing a new card from the deck!");
   let thirdCard = 4;
   sum += thirdCard;
-  startGame();
+  renderGame();
 }
