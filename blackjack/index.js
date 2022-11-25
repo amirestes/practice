71c7bc8a39b39379eb13c4160ce68b1fdb1ee3c5
Alice Mirestes <amirestes@gmail.com>
2022-11-24 20:49:23 -0600

    added isAlive variable

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,16 +1,19 @@
-// 1. Create two variables, firstCard and secondCard.
-// Set their values to a random number between 2-11
-
-// 2. Create a variable, sum, and set it to the sum of the two cards
-
 let firstCard = 6;
-let secondCard = 9;
+let secondCard = 11;
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
 
-if (sum < 21) {
+if (sum <= 20) {
   console.log("Do you want to draw a new card? 🙂");
 } else if (sum === 21) {
   console.log("Woohoo! You've got Blackjack! 🥳");
+  hasBlackJack = true;
 } else {
   console.log("You're out of the game! 😭");
+  isAlive = false;
 }
+
+//CASH OUT
+console.log(hasBlackJack);
+console.log(isAlive);
