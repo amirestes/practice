68684412c04db98649599ac2f4f1541083e63e94
Alice Mirestes <amirestes@gmail.com>
2022-12-02 17:05:08 -0600

    Added functional toggle switches for numbers and special characters

--- random password/index.js
+++ random password/index.js
@@ -92,7 +92,10 @@ const characters = [
   "/",
 ];
 let noSpecialCharacters = characters.slice(0, 62);
+let specialCharacters = characters.slice(62, 92);
 let noNumbers = characters.slice(52, 62);
+let lettersOnly = characters.slice(0, 52);
+let lettersCharacters = lettersOnly.concat(specialCharacters);
 let passwordOneEl = document.getElementById("password1-el");
 let passwordTwoEl = document.getElementById("password2-el");
 let characterInput = document.getElementById("characterInput");
@@ -100,21 +103,26 @@ let toggleSpecial = document.querySelector("#toggleSpecial");
 let toggleNumber = document.querySelector("#toggleNumber");
 
 function generatePassword() {
+  toggleSwitch();
+}
+
+const toggleSwitch = () => {
   let randomPasswordOne = [];
   let randomPasswordTwo = [];
-  let randomIndexOne = Math.floor(Math.random() * noSpecialCharacters.length);
-  let randomIndexTwo = Math.floor(Math.random() * noSpecialCharacters.length);
 
-  if (toggleSpecial.checked) {
+  //if both are off return random alphanumeric
+  if (!toggleNumber.checked && !toggleSpecial.checked) {
     for (let i = 0; i < characterInput.value; i++) {
-      randomIndexOne = Math.floor(Math.random() * characters.length);
-      randomIndexTwo = Math.floor(Math.random() * characters.length);
-      randomPasswordOne.push(characters[randomIndexOne]);
-      randomPasswordTwo.push(characters[randomIndexTwo]);
+      let randomIndexOne = Math.floor(Math.random() * lettersOnly.length);
+      let randomIndexTwo = Math.floor(Math.random() * lettersOnly.length);
+      randomPasswordOne.push(lettersOnly[randomIndexOne]);
+      randomPasswordTwo.push(lettersOnly[randomIndexTwo]);
       passwordOneEl.textContent = randomPasswordOne.join(" ");
       passwordTwoEl.textContent = randomPasswordTwo.join(" ");
     }
-  } else {
+    //this works
+  } else if (!toggleSpecial.checked) {
+    //if special off and number is on
     for (let i = 0; i < characterInput.value; i++) {
       randomIndexOne = Math.floor(Math.random() * noSpecialCharacters.length);
       randomIndexTwo = Math.floor(Math.random() * noSpecialCharacters.length);
@@ -123,40 +131,32 @@ function generatePassword() {
       passwordOneEl.textContent = randomPasswordOne.join(" ");
       passwordTwoEl.textContent = randomPasswordTwo.join(" ");
     }
+  } else if (!toggleNumber.checked) {
+    for (let i = 0; i < characterInput.value; i++) {
+      randomIndexOne = Math.floor(Math.random() * lettersCharacters.length);
+      randomIndexTwo = Math.floor(Math.random() * lettersCharacters.length);
+      randomPasswordOne.push(lettersCharacters[randomIndexOne]);
+      randomPasswordTwo.push(lettersCharacters[randomIndexTwo]);
+      passwordOneEl.textContent = randomPasswordOne.join(" ");
+      passwordTwoEl.textContent = randomPasswordTwo.join(" ");
+    } //this works
+  } else {
+    //if the special and number are toggled on THIS WORKS
+    for (let i = 0; i < characterInput.value; i++) {
+      randomIndexOne = Math.floor(Math.random() * characters.length);
+      randomIndexTwo = Math.floor(Math.random() * characters.length);
+      randomPasswordOne.push(characters[randomIndexOne]);
+      randomPasswordTwo.push(characters[randomIndexTwo]);
+      passwordOneEl.textContent = randomPasswordOne.join(" ");
+      passwordTwoEl.textContent = randomPasswordTwo.join(" ");
+    }
   }
-  // toggleNumberSelector();
-}
+};
 
 const reset = () => {
   passwordOneEl.textContent = "";
   passwordTwoEl.textContent = "";
   characterInput.value = "";
   toggleSpecial.checked = true;
+  toggleNumber.checked = true;
 };
-
-// const toggleNumberSelector = () => {
-//   let randomPasswordOne = [];
-//   let randomPasswordTwo = [];
-//   let randomIndexOne = Math.floor(Math.random() * noNumbers.length);
-//   let randomIndexTwo = Math.floor(Math.random() * noNumbers.length);
-
-//   if (toggleNumber.checked) {
-//     for (let i = 0; i < characterInput.value; i++) {
-//       randomIndexOne = Math.floor(Math.random() * characters.length);
-//       randomIndexTwo = Math.floor(Math.random() * characters.length);
-//       randomPasswordOne.push(characters[randomIndexOne]);
-//       randomPasswordTwo.push(characters[randomIndexTwo]);
-//       passwordOneEl.textContent = randomPasswordOne.join(" ");
-//       passwordTwoEl.textContent = randomPasswordTwo.join(" ");
-//     }
-//   } else {
-//     for (let i = 0; i < characterInput.value; i++) {
-//       randomIndexOne = Math.floor(Math.random() * noNumbers.length);
-//       randomIndexTwo = Math.floor(Math.random() * noNumbers.length);
-//       randomPasswordOne.push(noNumbers[randomIndexOne]);
-//       randomPasswordTwo.push(noNumbers[randomIndexTwo]);
-//       passwordOneEl.textContent = randomPasswordOne.join(" ");
-//       passwordTwoEl.textContent = randomPasswordTwo.join(" ");
-//     }
-//   }
-// };
