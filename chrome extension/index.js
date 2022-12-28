0bf3ab401d60211656ff22f8814f5e2c1c2857d9
Alice Mirestes <amirestes@gmail.com>
2022-12-27 22:16:29 -0600

    added localStorage to button

--- chrome extension/index.js
+++ chrome extension/index.js
@@ -1,23 +1,14 @@
-let myLeads = `["www.example.com"]`;
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
-// 1. Turn the myLeads string into an array
-myLeads = JSON.parse(myLeads);
-// 2. Push a new value to the array
-myLeads.push("www.ihoundz.com");
-console.log(myLeads);
-
-myLeads = JSON.stringify(myLeads);
-console.log(typeof myLeads);
-// 3. Turn the array into a string again
-// 4. Console.log the string using typeof to verify that it's a string
-
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   renderLeads();
   clearInput();
+  console.log(localStorage.getItem("myLeads"));
 });
 
 function renderLeads() {
