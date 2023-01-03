ca501f05e014f445515c1dbd038ebffa28b9fca9
Alice Mirestes <amirestes@gmail.com>
2023-01-02 21:54:08 -0600

    created function for tabBtn eventlistener

--- chrome extension/index.js
+++ chrome extension/index.js
@@ -14,7 +14,9 @@ if (leadsFromLocalStorage) {
 }
 
 tabBtn.addEventListener("click", function () {
-  console.log(tabs[0].url);
+  myLeads.push(tabs[0].url);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
 });
 
 function render(leads) {
