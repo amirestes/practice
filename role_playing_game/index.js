f4ac93dcaff2a198bfdfda1a733a18da7418cc09
Alice Mirestes <amirestes@gmail.com>
2023-03-09 12:08:48 -0600

    refactored getDiceRollArray using new Array constructor

--- role_playing_game/index.js
+++ role_playing_game/index.js
@@ -1,38 +1,40 @@
-/*
-Challenge: 
-1. Take the hard-coded HTML for the Wizard card, bring it 
-   into index.js and then inject it back into its div with 
-   JavaScript.
-2. Do the same for Orc card. 
-*/
+//Random Dice roll
+function getDiceRollArray(diceCount) {
+  return new Array(diceCount)
+    .fill(0)
+    .map(() => Math.floor(Math.random() * 6) + 1);
+}
+
+//Renders random dice to DOM
+function getDiceHtml(diceCount) {
+  return getDiceRollArray(diceCount)
+    .map((num) => `<div class="dice">${num}</div>`)
+    .join("");
+}
+
+//Hero object
 const hero = {
   elementId: "hero",
   name: "Wizard",
   avatar: "images/wizard.png",
   health: 60,
-  diceScore: [3, 1, 4],
   diceCount: 3,
 };
 
+//Monster object
 const monster = {
   elementId: "monster",
   name: "Orc",
   avatar: "images/orc.png",
   health: 10,
-  diceScore: [2],
   diceCount: 1,
 };
 
-renderCharacter(hero);
-renderCharacter(monster);
-
+//Renders the character to the DOM
 function renderCharacter(data) {
   const { elementId, name, avatar, health, diceScore, diceCount } = data;
 
   const diceHtml = getDiceHtml(diceCount);
-  //    diceScore
-  //  .map((dice) => `<div class="dice">${dice}</div>`)
-  //  .join("");
 
   document.getElementById(elementId).innerHTML = `<div class="character-card">
       <h4 class="name">${name}</h4>
@@ -44,18 +46,5 @@ function renderCharacter(data) {
    </div>`;
 }
 
-function getDiceRollArray(diceCount) {
-  const newDiceRolls = [];
-  for (let i = 0; i < diceCount; i++) {
-    newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
-  }
-  return newDiceRolls;
-}
-
-function getDiceHtml(diceCount) {
-  return getDiceRollArray(diceCount)
-    .map((num) => `<div class="dice">${num}</div>`)
-    .join("");
-}
-
-// getDiceRollArray(3);
+renderCharacter(hero);
+renderCharacter(monster);
