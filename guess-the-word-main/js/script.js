215053a9a01c96fe05fb37a728e7ab75a8b053cf
Alice Mirestes <amirestes@gmail.com>
2023-03-20 11:18:29 -0500

    wordInProgressCircle()

--- guess-the-word-main/js/script.js
+++ guess-the-word-main/js/script.js
@@ -3,6 +3,14 @@ const guessButton = document.getElementById("guess");
 const letterInput = document.getElementById("letter");
 const wordInProgress = document.getElementById("word-in-progress");
 const remaining = document.getElementById("remaining");
-const span = document.querySelector(span);
+// const span = document.querySelector(span);
 const message = document.getElementById("message");
 const playAgain = document.getElementById("play-again");
+
+const word = "magnolia";
+
+function wordInProgressCircle(word) {
+  wordInProgress.innerText = "●";
+}
+
+wordInProgressCircle(word);
