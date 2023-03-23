894689292f8e5baf763638a21fabab24964e4dc6
Alice Mirestes <amirestes@gmail.com>
2023-03-23 12:48:59 -0500

    cleaning up code

--- guess-the-word-main/js/1script.js
+++ guess-the-word-main/js/1script.js
@@ -5,35 +5,26 @@ const wordInProgress = document.querySelector(".word-in-progress");
 const remaining = document.querySelector(".remaining");
 // const span = document.querySelector(span);
 const playAgain = document.querySelector(".play-again");
-const message = document.querySelector(".message");
-
 const word = "magnolia";
-
+let letter = letterInput.value;
+const message = document.querySelector(".message");
 const guessedLetters = [];
 
-//converts the word to a string and replaces each character with a circle
-const wordInProgressCircle = () => {
-  //string convert to an array
-  //change the items in the array into a circle
-  const circleArray = word
-    .split("")
-    .map((letter) => {
-      return "●";
-    })
-    .join("");
-  // display in word in progress
-  wordInProgress.innerText = circleArray;
-};
-
-wordInProgressCircle(word);
-
+//submits the guessed letter from the input
 guessButton.addEventListener("click", (e) => {
   e.preventDefault();
-  let letter = letterInput.value;
-  const correctGuess = inputValue(letter);
+  inputValue(letter);
+
+  letterInput.value = "";
 });
 
-const inputValue = (input) => {
+
+wordInProgressCircle(word);
+makeGuess(letter);
+
+//validates the player's input
+
+function inputValue(input) {
   const acceptedLetter = /[a-zA-Z]/;
   input.length === 0
     ? (message.innerText = "Please enter a letter")
@@ -42,4 +33,16 @@ const inputValue = (input) => {
     : !input.match(acceptedLetter)
     ? (message.innerText = "Please use letter not a character.")
     : input;
+}
+
+//check guessed letter and move to guessed Array
+const makeGuess = (letter) => {
+  letter = letter.toUpperCase();
+  //if the letter is not in the guessed array then i want to add it to the guessed array
+  if (!guessedLetters.includes(letter)) {
+    guessedLetters.push(letter);
+    console.log(guessedLetters);
+  } else {
+    message.innerText = "Letter already guessed. Try again.";
+  }
 };
