9ff124ca3d56b65dfcd691ff945e911ac2b22d1a
Alice Mirestes <amirestes@gmail.com>
2023-03-22 20:10:53 -0500

    created ternery

--- guess-the-word-main/js/script.js
+++ guess-the-word-main/js/script.js
@@ -9,14 +9,28 @@ const playAgain = document.querySelector(".play-again");
 
 const word = "magnolia";
 
+//submits the guessed letter from the input
 guessButton.addEventListener("click", (e) => {
   e.preventDefault();
   const letter = letterInput.value;
   console.log(letter);
   letterInput.value = "";
+  inputValue(letterInput.value);
 });
 
-function wordInProgressCircle() {
+//validates the player's input
+function inputValue(input) {
+  const acceptedLetter = /[a-zA-Z]/;
+  input.length === 0
+    ? (message.innerText = "Please enter a letter")
+    : input.length > 1
+    ? (message.innerText = "Only enter one letter")
+    : input.match(acceptedLetter)
+    ? (message.innerText = "Please use letter not a character.")
+    : input;
+}
+//converts the word to a string and replaces each character with a circle
+const wordInProgressCircle = () => {
   //string convert to an array
   const wordArray = word.split("");
   //change the items in the array into a circle
@@ -27,6 +41,6 @@ function wordInProgressCircle() {
     .join("");
   // display in word in progress
   wordInProgress.innerText = circleArray;
-}
+};
 
 wordInProgressCircle(word);
