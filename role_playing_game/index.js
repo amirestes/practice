b4ac3a72863188800e9f724729f724be39560393
Alice Mirestes <amirestes@gmail.com>
2023-03-29 19:03:27 -0500

    added endEmoji

--- role_playing_game/index.js
+++ role_playing_game/index.js
@@ -2,12 +2,6 @@ import characterData from "./data.js";
 import { Character } from "./Character.js";
 import { getDiceRollArray, getDicePlaceholderHtml } from "./utils.js";
 
-//Renders character to DOM
-function render() {
-  document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
-  document.getElementById("monster").innerHTML = orc.getCharacterHtml();
-}
-
 function attack() {
   wizard.getDiceHtml();
   orc.getDiceHtml();
@@ -19,7 +13,7 @@ function attack() {
   }
 }
 
-function endGame() {\
+function endGame() {
   const endMessage =
     wizard.health === 0 && orc.health === 0
       ? "No victors - all creatures are dead"
@@ -27,10 +21,25 @@ function endGame() {\
       ? "The Wizard is Victorious"
       : "The Orc is Victorious";
 
-  console.log(endMessage);
+  // console.log(endMessage);
+
+  const endEmoji = wizard.health > 0 ? "🔮" : "☠️";
+
+  document.body.innerHTML = `<div class="end-game">
+        <h2>Game Over</h2>
+        <h3>${endMessage}</h3>
+        <p class="end-emoji">${endEmoji}</p>
+    </div>`;
 }
 
 document.getElementById("attack-button").addEventListener("click", attack);
+
+//Renders character to DOM
+function render() {
+  document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
+  document.getElementById("monster").innerHTML = orc.getCharacterHtml();
+}
+
 const wizard = new Character(characterData.hero);
 const orc = new Character(characterData.monster);
 render();
