cc5b66daf5a8428d282caccbbc4da0836a19936a
Alice Mirestes <amirestes@gmail.com>
2023-04-27 22:19:26 -0500

    added more monsters to the data file

--- role_playing_game/data.js
+++ role_playing_game/data.js
@@ -8,12 +8,27 @@ const characterData = {
     currentDiceScore: [],
   },
 
-  //Monster object
-  monster: {
+  //Monster objects
+
+  orc: {
     name: "Orc",
     avatar: "images/orc.png",
     health: 40,
     diceCount: 1,
+    currentDiceScore: [],
+  },
+  demon: {
+    name: "Demon",
+    avatar: "images/demon.png",
+    health: 25,
+    diceCount: 2,
+    currentDiceScore: [],
+  },
+  goblin: {
+    name: "Goblin",
+    avatar: "images/goblin.png",
+    health: 20,
+    diceCount: 3,
     currentDiceScore: [],
   },
 };
