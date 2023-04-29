8db1f61ea510d6ee1706a03311e0ed8bf3b915c6
Alice Mirestes <amirestes@gmail.com>
2023-04-28 22:59:11 -0500

    added if/else statements to iterate through the monstersArray

--- role_playing_game/index.js
+++ role_playing_game/index.js
@@ -1,30 +1,39 @@
 import characterData from "./data.js";
 import Character from "./Character.js";
-// import { getDiceRollArray, getDicePlaceholderHtml } from "./utils.js";
+
 let monstersArray = ["orc", "demon", "goblin"];
 
 function getNewMonster() {
   const nextMonsterData = characterData[monstersArray.shift()];
+  return nextMonsterData ? new Character(nextMonsterData) : {};
 }
 
 function attack() {
   wizard.getDiceHtml();
-  orc.getDiceHtml();
-  wizard.takeDamage(orc.currentDiceScore);
-  orc.takeDamage(wizard.currentDiceScore);
+  monster.getDiceHtml();
+  wizard.takeDamage(monster.currentDiceScore);
+  monster.takeDamage(wizard.currentDiceScore);
   render();
-  if (wizard.dead || orc.dead) {
+
+  if (wizard.dead) {
     endGame();
+  } else if (monster.dead) {
+    if (monstersArray.length > 0) {
+      monster = getNewMonster();
+      render();
+    } else {
+      endGame();
+    }
   }
 }
 
 function endGame() {
   const endMessage =
-    wizard.health === 0 && orc.health === 0
+    wizard.health === 0 && monster.health === 0
       ? "No victors - all creatures are dead"
       : wizard.health > 0
-      ? "The Wizard is Victorious"
-      : "The Orc is Victorious";
+      ? "The Wizard Wins"
+      : `The ${monster.name} is Victorious`;
 
   const endEmoji = wizard.health > 0 ? "🔮" : "☠️";
   document.body.innerHTML = `<div class="end-game">
@@ -39,9 +48,9 @@ document.getElementById("attack-button").addEventListener("click", attack);
 //Renders character to DOM
 function render() {
   document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
-  document.getElementById("monster").innerHTML = orc.getCharacterHtml();
+  document.getElementById("monster").innerHTML = monster.getCharacterHtml();
 }
 
 const wizard = new Character(characterData.hero);
-const orc = new Character(characterData.monster);
+let monster = getNewMonster();
 render();
