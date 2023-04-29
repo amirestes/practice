05f8a294c9b80ec327ec916e8dadee0608307350
Alice Mirestes <amirestes@gmail.com>
2023-04-28 23:24:19 -0500

    added setTimeOut()

--- role_playing_game/index.js
+++ role_playing_game/index.js
@@ -19,8 +19,10 @@ function attack() {
     endGame();
   } else if (monster.dead) {
     if (monstersArray.length > 0) {
-      monster = getNewMonster();
-      render();
+      setTimeout(() => {
+        monster = getNewMonster();
+        render();
+      }, 1500);
     } else {
       endGame();
     }
@@ -36,11 +38,13 @@ function endGame() {
       : `The ${monster.name} is Victorious`;
 
   const endEmoji = wizard.health > 0 ? "🔮" : "☠️";
-  document.body.innerHTML = `<div class="end-game">
-        <h2>Game Over</h2>
-        <h3>${endMessage}</h3>
-        <p class="end-emoji">${endEmoji}</p>
-    </div>`;
+  setTimeout(() => {
+    document.body.innerHTML = `<div class="end-game">
+      <h2>Game Over</h2>
+      <h3>${endMessage}</h3>
+      <p class="end-emoji">${endEmoji}</p>
+      </div>`;
+  }, 1500);
 }
 
 document.getElementById("attack-button").addEventListener("click", attack);
