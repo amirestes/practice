32c75667d26833cdfb14dca451e64c471362b66d
Alice Mirestes <amirestes@gmail.com>
2023-06-11 23:52:06 -0500

    added global variables

---
+++ guess-word/js/script.js
@@ -0,0 +1,8 @@
+const message = document.querySelector(".message");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const playAgainButton = document.querySelector(".play-again");
+
+let word = "magnolia";
