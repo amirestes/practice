06e5b150ce99f3ad2b613e8305a303495c2358f4
Alice Mirestes <amirestes@gmail.com>
2023-06-12 11:56:42 -0500

    created dot function

--- guess-word/js/script.js
+++ guess-word/js/script.js
@@ -6,3 +6,22 @@ const guessButton = document.querySelector(".guess");
 const playAgainButton = document.querySelector(".play-again");
 
 let word = "magnolia";
+
+/* 
+    Write a function that will create a blank array, then take the variable word and convert the string into the blank array. Display the array as a dot in the word-in-progress.
+*/
+
+function dot(word) {
+  //create a blank array
+  let wordArray = [];
+  //Convert word to an array
+  wordArray = word.split("");
+  //convert word to an array to obtain the array length
+  let originalWord = word.split("");
+  //Map over the array to display a dot in the word-in-progress element
+  wordArray.map(() => wordArray.splice(wordArray.length, -[wordArray], "●"));
+  //Displays the array in the word-in-progress element by using .slice to remove the original word from displaying by using the originalWord array to obtain the length
+  wordInProgress.innerText = wordArray.slice(originalWord.length).join("");
+}
+
+dot(word);
---
+++ guess-word/readme.md
@@ -0,0 +1,6 @@
+Created a previous version of this app in vanilla JavaScript.
+
+Notes on what I changed.
+
+In the original version I used a for...of loop in the circle function. In this version I mapped over the array to replace the letter of the word with a ●.
+
