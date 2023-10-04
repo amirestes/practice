696dbd33b39a4e90a902e1395fe5cd56d81a6507
Alice Mirestes <amirestes@gmail.com>
2023-10-04 00:26:54 -0500

    hooks practice 4

---
+++ 302-l02-challenge-01-starter-forked/302-l02-practice-exercise-02-starter-forked/src/Food.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const Food = ({ name }) => {
+  const [tried, setTried] = useState(false);
+  return (
+    <li className={tried ? "strike" : ""} onClick={() => setTried(!tried)}>
+      {name}
+    </li>
+  );
+};
+
+export default Food;
