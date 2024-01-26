267ef95d147a5d0012f8f0ad1784cbfe482408f6
Alice Mirestes <amirestes@gmail.com>
2024-01-25 22:53:05 -0600

    added dark mode toggle

--- unit_conversion/js/index.js
+++ unit_conversion/js/index.js
@@ -1,1 +1,14 @@
 const main = document.getElementById("main");
+const toggle = document.getElementById("toggle");
+
+const toggleSwitch = () => {
+  if (!toggleSpecial.checked) {
+    main.classList.remove("dark");
+    main.classList.add("light");
+  } else {
+    main.classList.add("dark");
+    main.classList.remove("light");
+  }
+};
+
+toggleSwitch();
