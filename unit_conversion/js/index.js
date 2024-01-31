6d3be231badca600da31ae7e82afb5b8f11bfcb2
Alice Mirestes <amirestes@gmail.com>
2024-01-30 18:45:06 -0600

    converted values and print values

--- unit_conversion/js/index.js
+++ unit_conversion/js/index.js
@@ -5,10 +5,17 @@ const toggle = document.getElementById("toggle");
 const characterInput = document.getElementById("unit");
 const wrongCharacter = document.querySelector("h3");
 const numSpan = document.querySelectorAll(".number");
+const ftSpan = document.querySelector("#feet");
+const meterSpan = document.querySelector("#meters");
+const litSpan = document.querySelector("#gallons");
+const galSpan = document.querySelector("#liters");
+const kilSpan = document.querySelector("#kilos");
+const poundSpan = document.querySelector("#pounds");
 
 //onclick function for the Convert button
 const clickMe = () => {
   validateInput();
+  convertValue();
   characterInput.value = "";
 };
 
@@ -37,12 +44,39 @@ const validateInput = () => {
   if (cI.match(/[a-zA-Z$&+,:;=?@#|'<>.^*()%!-]/)) {
     wrongCharacter.innerText = `Please enter a number`;
   } else {
-    console.log(cI);
     for (let i = 0; i < numSpan.length; i++) {
       numSpan[i].innerText = cI;
     }
     wrongCharacter.innerText = "";
   }
+};
+
+const convertValue = () => {
+  let cI = characterInput.value;
+
+  //meter to foot conversion
+  let meterToFoot = cI / 0.3048;
+  ftSpan.innerText = meterToFoot.toFixed(3);
+
+  //foot to meter conversion
+  let footToMeter = cI / 3.28084;
+  meterSpan.innerText = footToMeter.toFixed(3);
+
+  //Liters to gallons
+  let litToGal = cI / 3.78541;
+  litSpan.innerText = litToGal.toFixed(3);
+
+  //gallons to liters conversion
+  let galToLit = cI * 3.78541;
+  galSpan.innerText = galToLit.toFixed(3);
+
+  //kilos to pounds
+  let kilToPound = cI / 0.453592;
+  kilSpan.innerText = kilToPound.toFixed(3);
+
+  //pounds to kilos
+  let poundsToKilos = cI * 0.453592;
+  poundSpan.innerText = poundsToKilos.toFixed(3);
 };
 
 toggleSwitch();
