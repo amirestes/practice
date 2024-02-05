561019785564a4c98ad35f9ad1c608b2eea1fe26
Alice Mirestes <amirestes@gmail.com>
2024-02-05 09:46:31 -0600

    setup Firebase import/export, and added push function to event listener

--- firebase_app/index.js
+++ firebase_app/index.js
@@ -1,12 +1,23 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
-const addBtn = document.getElementById("add-button")
-const input = document.getElementById("input-field")
+const appSettings = {
+  databaseURL: "https://realtime-database-34a94-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
+
+const addBtn = document.getElementById("add-button");
+const input = document.getElementById("input-field");
 
 addBtn.addEventListener("click", () => {
-    let inputValue = input.value
-    console.log(inputValue)
-})
+  let inputValue = input.value;
+  push(shoppingListInDB, inputValue);
+  console.log(inputValue);
+});
